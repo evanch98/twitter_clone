cb64493b315474133d01e93145767591a8794afb
Kyaw Thu <evanch98@gmail.com>
2023-04-20 21:10:46 +0800

    initialize ReusableAppBar

---
+++ lib/common/reusable_appbar.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ReusableAppBar extends StatelessWidget {
+  const ReusableAppBar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
