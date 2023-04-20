4c7ac48504543b004ae68c7fc21a7ed88929ad7f
Kyaw Thu <evanch98@gmail.com>
2023-04-20 21:19:01 +0800

    add AppBar that contains twitterLogo as a title

--- lib/common/reusable_appbar.dart
+++ lib/common/reusable_appbar.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:twitter_clone/constants/assets_constants.dart';
 
 class ReusableAppBar extends StatelessWidget {
   const ReusableAppBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return AppBar(
+      title: SvgPicture.asset(
+        AssetsConstants.twitterLogo,
+      ),
+    );
   }
 }
-
