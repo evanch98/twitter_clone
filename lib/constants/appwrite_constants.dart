07aa509af670c41917f70661c9b7080122ed56bd
Kyaw Thu <evanch98@gmail.com>
2023-04-20 20:14:20 +0800

    Add appwrite databaseId, projectId, and endPoint

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId ='64412b9fb92dd3cf577d';  // appwrite database Id
+  static const String projectId = '64411c1330c31bade6b5';  // appwrite project Id
+  static const String endPoint = 'http://localhost:80/v1';  // localhost port 80
+}
