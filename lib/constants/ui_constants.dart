38febff63300991852486dd6ef2b3da2b3baf9bc
Kyaw Thu <evanch98@gmail.com>
2023-04-20 21:38:21 +0800

    change color and height of the twitterLogo and set the centerTitle to true

--- lib/constants/ui_constants.dart
+++ lib/constants/ui_constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class UIConstants {
   // to be able to use in the Scaffold appBar because appBar does not accept
@@ -10,7 +11,11 @@ class UIConstants {
     return AppBar(
       title: SvgPicture.asset(
         AssetsConstants.twitterLogo,
+        // to change the color of the vector
+        colorFilter: const ColorFilter.mode(Pallete.blueColor, BlendMode.srcIn),
+        height: 30,
       ),
+      centerTitle: true,
     );
   }
 }
