bc2048005f9cfc78fad82eb0b4dcee368630b1b2
Kyaw Thu <evanch98@gmail.com>
2023-04-21 21:34:54 +0800

    change the Success data type to more generic data type

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -1,4 +1,5 @@
 import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/core/failure.dart';
 
 // typedef simply allows us to create our own type definition
-typedef FutureEither = Future<Either<Failure, Success>>;
+typedef FutureEither<T> = Future<Either<Failure, T>>;
