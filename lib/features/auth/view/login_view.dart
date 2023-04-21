cd39ee32fbff3c4fa32bdd407e6dfdce72965109
Kyaw Thu <evanch98@gmail.com>
2023-04-21 17:41:46 +0800

    optimize imports

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:twitter_clone/common/rounded_small_button.dart';
+import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/features/auth/widgets/auth_field.dart';
 import 'package:twitter_clone/theme/theme.dart';
