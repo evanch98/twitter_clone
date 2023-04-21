ce163dff6b6a4dae43d5b575436a355aef77cd2a
Kyaw Thu <evanch98@gmail.com>
2023-04-21 22:10:47 +0800

    add comments for the future reference and the correct data type for the FutureEither Success datatype

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,10 +1,15 @@
 import 'package:appwrite/appwrite.dart';
+import 'package:appwrite/models.dart' as model;
 import 'package:twitter_clone/core/core.dart';
+
+// to signup, use Account from appwrite.dart (to get user accounts)
+// to access user related data, use Account from models.dart
 
 // interface for the AuthAPI class
 abstract class IAuthAPI {
-  // Account datatype of appwrite.dart from the appwrite package
-  FutureEither<Account> signUp({
+  // the reason to use model.Account is that after signing up, the method should
+  // return the user's account model (user related data)
+  FutureEither<model.Account> signUp({
     required String email,
     required String password,
   });
