b58d5493f4f6e9f1c3bb164b07c1d5dd90602618
Kyaw Thu <evanch98@gmail.com>
2023-04-21 16:48:08 +0800

    add comments for the future reference

--- lib/features/auth/widgets/auth_field.dart
+++ lib/features/auth/widgets/auth_field.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
+// AuthField will only be used in authentication related text fields
 class AuthField extends StatelessWidget {
   final TextEditingController controller;
   final String hintText;
@@ -16,6 +17,7 @@ class AuthField extends StatelessWidget {
     return TextFormField(
       controller: controller,
       decoration: InputDecoration(
+        // when the text field is focused
         focusedBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5),
           borderSide: const BorderSide(
@@ -23,12 +25,14 @@ class AuthField extends StatelessWidget {
             width: 3,
           ),
         ),
+        // when the text field is not focused
         enabledBorder: OutlineInputBorder(
           borderRadius: BorderRadius.circular(5),
           borderSide: const BorderSide(
             color: Pallete.greyColor,
           ),
         ),
+        // padding inside the text field
         contentPadding: const EdgeInsets.all(22),
         hintText: hintText,
         // to increase the overall size of the text field
