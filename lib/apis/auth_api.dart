68bf6602158faa37421e19504dabf2437d7e1a18
Kyaw Thu <evanch98@gmail.com>
2023-04-21 21:51:14 +0800

    add comments for the future reference

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -3,6 +3,7 @@ import 'package:twitter_clone/core/core.dart';
 
 // interface for the AuthAPI class
 abstract class IAuthAPI {
+  // Account datatype of appwrite.dart from the appwrite package
   FutureEither<Account> signUp({
     required String email,
     required String password,
