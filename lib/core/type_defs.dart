9f25da00d2617e44f09f7e86f4272be62145cb17
Kyaw Thu <evanch98@gmail.com>
2023-04-21 21:44:03 +0800

    add the FutureVoid type definition

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -10,3 +10,4 @@ import 'package:twitter_clone/core/failure.dart';
 typedef FutureEither<T> = Future<Either<Failure, T>>;
 // FutureEitherVoid will return nothing when there is a success
 typedef FutureEitherVoid = FutureEither<void>;
+typedef FutureVoid = Future<void>;
