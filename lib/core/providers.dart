c5bc7ab1bad1b82fb6050f12b83a472b1f55860a
Kyaw Thu <evanch98@gmail.com>
2023-04-22 00:47:22 +0800

    add appwriteClientProvider and appwriteAccountProvider

---
+++ lib/core/providers.dart
@@ -0,0 +1,21 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/constants/constants.dart';
+
+final appwriteClientProvider = Provider((ref) {
+  Client client = Client();
+  return client
+      .setEndpoint(AppwriteConstants.endPoint)
+      .setProject(AppwriteConstants.projectId)
+      .setSelfSigned(status: true); // selfSigned should only be used in the development
+});
+
+final appwriteAccountProvider = Provider((ref) {
+  // ref allows us to communicate with other providers
+  // ref.watch means it will continuously watching any changes that will happen
+  // to appwriteClientProvider
+  // ref.read can also be used, but it will only read once and won't update any
+  // changes
+  final client = ref.watch(appwriteClientProvider);
+  return Account(client);
+});
