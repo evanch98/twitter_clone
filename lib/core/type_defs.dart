bdda2312eb8f6824507063eea8f6a38e3535e32b
Kyaw Thu <evanch98@gmail.com>
2023-04-21 21:41:37 +0800

    add comments for the future reference

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -4,4 +4,7 @@ import 'package:twitter_clone/core/failure.dart';
 // typedef simply allows us to create our own type definition
 // Either lets allows us to return two data types from a function (from the
 // fpdart package).
+
+// FutureEither will return the data type Failure when there is a failure
+// FutureEither will return the appropriate data type when there is a success
 typedef FutureEither<T> = Future<Either<Failure, T>>;
