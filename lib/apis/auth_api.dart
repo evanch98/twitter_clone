a20bafbd637dab91d0b34732c21e6fcce02c2747
Kyaw Thu <evanch98@gmail.com>
2023-04-21 21:37:05 +0800

    update IAuthAPI

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -1,10 +1,9 @@
-import 'package:fpdart/fpdart.dart';
+import 'package:appwrite/appwrite.dart';
+import 'package:twitter_clone/core/core.dart';
 
 // interface for the AuthAPI class
 abstract class IAuthAPI {
-  // Either lets allows us to return two data types from a function (from the
-  // fpdart package).
-  Future<Either<int, String>> signUp({
+  FutureEither<Account> signUp({
     required String email,
     required String password,
   });
