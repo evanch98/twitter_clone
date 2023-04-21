8037ce30668cf2bab9c781362a784962f055d466
Kyaw Thu <evanch98@gmail.com>
2023-04-21 17:10:00 +0800

    implement RoundedSmallButton

---
+++ lib/common/rounded_small_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/theme/theme.dart';
+
+class RoundedSmallButton extends StatelessWidget {
+  final VoidCallback onTap;
+  final String label;
+  final Color backgroundColor;
+  final Color textColor;
+
+  const RoundedSmallButton({
+    Key? key,
+    required this.onTap,
+    required this.label,
+    this.backgroundColor = Pallete.whiteColor,
+    this.textColor = Pallete.backgroundColor,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Chip(
+      label: Text(
+        label,
+        style: TextStyle(color: textColor, fontSize: 16),
+      ),
+      backgroundColor: backgroundColor,
+      labelPadding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 5,
+      ),
+    );
+  }
+}
