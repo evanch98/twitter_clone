65388a05d2776c808ef8f1881d2cd864ae41848e
Kyaw Thu <evanch98@gmail.com>
2023-04-22 01:38:56 +0800

    wrap the main app widget with the ProviderScope widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/features/auth/view/login_view.dart';
 import 'package:twitter_clone/features/auth/view/signup_view.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
