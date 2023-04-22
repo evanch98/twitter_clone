cf8120fb5d26a4d0cdc6010025f09833e2e84fd6
Kyaw Thu <evanch98@gmail.com>
2023-04-23 00:57:52 +0800

    initialize HomeView

---
+++ lib/features/home/view/home_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class HomeView extends ConsumerWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return const Scaffold();
+  }
+}
+
