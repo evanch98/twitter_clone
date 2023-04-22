a22b7376d250d917ae6a9ba036db768fb6d58b24
Kyaw Thu <evanch98@gmail.com>
2023-04-23 01:30:15 +0800

    change the MyApp widget from the StatelessWidget to the ConsumerWidget

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,11 @@ void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends ConsumerWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: AppTheme.theme,
