c4d558727d7a1380dffd75a1ff9d301f51a3c5cf
Kyaw Thu <evanch98@gmail.com>
2023-04-22 21:25:19 +0800

    add a stateless widget Loader that builds CircularProgressIndicator()

---
+++ lib/common/loading_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Loader extends StatelessWidget {
+  const Loader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+}
+
