2c30acd22a07b7232e104ef7bb5429d25d263c47
Kyaw Thu <evanch98@gmail.com>
2023-04-23 00:47:45 +0800

    add the loading functionality when the server is loading

--- lib/features/auth/view/login_view.dart
+++ lib/features/auth/view/login_view.dart
@@ -48,80 +48,83 @@ class _LoginViewState extends ConsumerState<LoginView> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authControllerProvider);
     return Scaffold(
       appBar: appbar,
-      body: Center(
-        // SingleChildScrollView is to make sure that when the virtual keyboard
-        // pops up, it does not cause the out-of-pixels problem with the widgets.
-        child: SingleChildScrollView(
-          child: Padding(
-            // horizontal padding
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-            ),
-            child: Column(
-              children: [
-                // Email TextField
-                AuthField(
-                  controller: emailController,
-                  hintText: "Email",
-                ),
-                const SizedBox(
-                  height: 25,
-                ), // space between the two text fields
-                // Password TextField
-                AuthField(
-                  controller: passwordController,
-                  hintText: "Password",
-                ),
-                const SizedBox(
-                  height: 40,
-                ), // space between the two widgets
-                Align(
-                  // to align the widget to the right side of the screen
-                  alignment: Alignment.topRight,
-                  child: RoundedSmallButton(
-                    onTap: onLogin,
-                    label: "Done",
+      body: isLoading
+          ? const Loader()
+          : Center(
+              // SingleChildScrollView is to make sure that when the virtual keyboard
+              // pops up, it does not cause the out-of-pixels problem with the widgets.
+              child: SingleChildScrollView(
+                child: Padding(
+                  // horizontal padding
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
                   ),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                // use RichText to be able to use TextSpan
-                RichText(
-                  // use TextSpan to have text with different colors in the same
-                  // line, it is more efficient to do it this way.
-                  text: TextSpan(
-                    text: "Don't have an account?",
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
+                  child: Column(
                     children: [
-                      TextSpan(
-                        text: " Sign up",
-                        style: const TextStyle(
-                          color: Pallete.blueColor,
-                          fontSize: 16,
+                      // Email TextField
+                      AuthField(
+                        controller: emailController,
+                        hintText: "Email",
+                      ),
+                      const SizedBox(
+                        height: 25,
+                      ), // space between the two text fields
+                      // Password TextField
+                      AuthField(
+                        controller: passwordController,
+                        hintText: "Password",
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ), // space between the two widgets
+                      Align(
+                        // to align the widget to the right side of the screen
+                        alignment: Alignment.topRight,
+                        child: RoundedSmallButton(
+                          onTap: onLogin,
+                          label: "Done",
                         ),
-                        // to be able to tap on the sign up part (to navigate to
-                        // other screens
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.push(
-                              context,
-                              SignUpView.route(),
-                            );
-                          },
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      // use RichText to be able to use TextSpan
+                      RichText(
+                        // use TextSpan to have text with different colors in the same
+                        // line, it is more efficient to do it this way.
+                        text: TextSpan(
+                          text: "Don't have an account?",
+                          style: const TextStyle(
+                            fontSize: 16,
+                          ),
+                          children: [
+                            TextSpan(
+                              text: " Sign up",
+                              style: const TextStyle(
+                                color: Pallete.blueColor,
+                                fontSize: 16,
+                              ),
+                              // to be able to tap on the sign up part (to navigate to
+                              // other screens
+                              recognizer: TapGestureRecognizer()
+                                ..onTap = () {
+                                  Navigator.push(
+                                    context,
+                                    SignUpView.route(),
+                                  );
+                                },
+                            ),
+                          ],
+                        ),
                       ),
                     ],
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-        ),
-      ),
     );
   }
 }
