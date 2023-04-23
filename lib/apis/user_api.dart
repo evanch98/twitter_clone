e17d9da426c3779b435fd2f87067188d6ad7185a
Kyaw Thu <evanch98@gmail.com>
2023-04-23 23:19:25 +0800

    add a parameter of UserModel datatype to the saveUserData method

--- lib/apis/user_api.dart
+++ lib/apis/user_api.dart
@@ -1,5 +1,6 @@
 import 'package:twitter_clone/core/core.dart';
+import 'package:twitter_clone/models/models.dart';
 
 abstract class IUserAPI {
-  FutureEitherVoid saveUserData();
+  FutureEitherVoid saveUserData(UserModel userModel);
 }
