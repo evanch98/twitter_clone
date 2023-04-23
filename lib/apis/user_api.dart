4d3d3f0ecca73bcd5e7fca155fa86c2c4aed3a25
Kyaw Thu <evanch98@gmail.com>
2023-04-23 23:45:08 +0800

    add UserAPI and implements IUserAPI

--- lib/apis/user_api.dart
+++ lib/apis/user_api.dart
@@ -1,6 +1,43 @@
+import 'package:appwrite/appwrite.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/core/core.dart';
 import 'package:twitter_clone/models/models.dart';
 
+// interface for the UserAPI class
 abstract class IUserAPI {
   FutureEitherVoid saveUserData(UserModel userModel);
+}
+
+class UserAPI implements IUserAPI {
+  final Databases _db;
+
+  UserAPI({required Databases db}) : _db = db;
+
+  @override
+  FutureEitherVoid saveUserData(UserModel userModel) async {
+    try {
+      await _db.createDocument(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.usersCollection,
+        documentId: ID.unique(),
+        data: userModel.toMap(),
+      );
+      return right(null);
+    } on AppwriteException catch (e, st) {
+      return left(
+        Failure(
+          e.message ?? "Some unexpected error occurred",
+          st,
+        ),
+      );
+    } catch (e, st) {
+      return left(
+        Failure(
+          e.toString(),
+          st,
+        ),
+      );
+    }
+  }
 }
