810fcdcf8971056c4620a33fd281614c4c81f1c5
Kyaw Thu <evanch98@gmail.com>
2023-04-23 23:39:23 +0800

    add usersCollection Id

--- lib/constants/appwrite_constants.dart
+++ lib/constants/appwrite_constants.dart
@@ -2,4 +2,6 @@ class AppwriteConstants {
   static const String databaseId ='64412b9fb92dd3cf577d';  // appwrite database Id
   static const String projectId = '64411c1330c31bade6b5';  // appwrite project Id
   static const String endPoint = 'http://localhost:80/v1';  // localhost port 80
+
+  static const String usersCollection = '644550ab7e2edb94d48b';  // appwrite usersCollection from database Id
 }
