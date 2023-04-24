9d60f518c2fc2eff4537832b301a169eaaed1c69
Kyaw Thu <evanch98@gmail.com>
2023-04-24 22:13:01 +0800

    change the HomeView to the StatefulWidget

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeView extends ConsumerWidget {
+class HomeView extends StatefulWidget {
   static route() => MaterialPageRoute(
-        builder: (context) => const HomeView(),
-      );
+    builder: (context) => const HomeView(),
+  );
 
   const HomeView({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
     return const Scaffold();
   }
 }
