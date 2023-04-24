998b1db85e08cd0ce4303221a4396e4ef1aa7aea
Kyaw Thu <evanch98@gmail.com>
2023-04-24 22:37:01 +0800

    add bottomNavigationBar containing home, search, and notification

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,11 +1,14 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class HomeView extends StatefulWidget {
   static route() => MaterialPageRoute(
-    builder: (context) => const HomeView(),
-  );
+        builder: (context) => const HomeView(),
+      );
 
   const HomeView({Key? key}) : super(key: key);
 
@@ -20,6 +23,38 @@ class _HomeViewState extends State<HomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar,
+      bottomNavigationBar: CupertinoTabBar(
+        backgroundColor: Pallete.backgroundColor,
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.homeFilledIcon,
+              colorFilter: const ColorFilter.mode(
+                Pallete.whiteColor,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.searchIcon,
+              colorFilter: const ColorFilter.mode(
+                Pallete.whiteColor,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              AssetsConstants.notifFilledIcon,
+              colorFilter: const ColorFilter.mode(
+                Pallete.whiteColor,
+                BlendMode.srcIn,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
