836a0fc5a6fbc65da36f018bc3bb0955569a7f55
Kyaw Thu <evanch98@gmail.com>
2023-04-24 22:51:27 +0800

    add the color of the icon of the FloatingActionButton

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -36,7 +36,10 @@ class _HomeViewState extends State<HomeView> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
-        child: const Icon(Icons.add),
+        child: const Icon(
+          Icons.add,
+          color: Pallete.whiteColor,
+        ),
       ),
       bottomNavigationBar: CupertinoTabBar(
         currentIndex: _page,
