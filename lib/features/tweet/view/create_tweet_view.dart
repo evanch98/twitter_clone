4465a1dad33c995d2ac009345cffa6e7931ecc2a
Kyaw Thu <evanch98@gmail.com>
2023-04-25 11:37:52 +0800

    add CreateTweetScreen widget which is a ConsumerStatefulWidget

---
+++ lib/features/tweet/view/create_tweet_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CreateTweetScreen extends ConsumerStatefulWidget {
+  const CreateTweetScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  ConsumerState createState() => _CreateTweetScreenState();
+}
+
+class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
