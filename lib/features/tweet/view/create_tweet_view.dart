6a0c207e0ecdf65999ea2703736dbc0b237952f6
Kyaw Thu <evanch98@gmail.com>
2023-04-25 11:53:07 +0800

    add the Scaffold's actions

--- lib/features/tweet/view/create_tweet_view.dart
+++ lib/features/tweet/view/create_tweet_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class CreateTweetScreen extends ConsumerStatefulWidget {
   static route() => MaterialPageRoute(
@@ -26,6 +28,14 @@ class _CreateTweetScreenState extends ConsumerState<CreateTweetScreen> {
             size: 30,
           ),
         ),
+        actions: [
+          RoundedSmallButton(
+            onTap: () {},
+            label: "Tweet",
+            backgroundColor: Pallete.blueColor,
+            textColor: Pallete.whiteColor,
+          ),
+        ],
       ),
     );
   }
