1abafa9047a8fd4f42a0f81155434ebfe06f864f
Kyaw Thu <evanch98@gmail.com>
2023-04-27 01:25:48 +0800

    implement ITweetAPI and its abstract method shareTweet

--- lib/apis/tweet_api.dart
+++ lib/apis/tweet_api.dart
@@ -1,8 +1,41 @@
+import 'package:appwrite/appwrite.dart';
 import 'package:appwrite/models.dart' as model;
+import 'package:fpdart/fpdart.dart';
+import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/core/core.dart';
 import 'package:twitter_clone/models/models.dart';
 
 // interface for the TweetAPI class
 abstract class ITweetAPI {
   FutureEither<model.Document> shareTweet(Tweet tweet);
+}
+
+class TweetAPI implements ITweetAPI {
+  final Databases _db;
+
+  TweetAPI({required Databases db}) : _db = db;
+
+  @override
+  FutureEither<model.Document> shareTweet(Tweet tweet) async {
+    try {
+      final document = await _db.createDocument(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.tweetsCollection,
+        documentId: ID.unique(),
+        data: tweet.toMap(),
+      );
+      return right(document);
+    } on AppwriteException catch (e, st) {
+      return left(
+        Failure(
+          e.message ?? 'Some unexpected error occurred',
+          st,
+        ),
+      );
+    } catch (e, st) {
+      return left(
+        Failure(e.toString(), st)
+      );
+    }
+  }
 }
