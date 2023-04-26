4015b1dc6f6b70153cf8dc861ae284dae9a90cb7
Kyaw Thu <evanch98@gmail.com>
2023-04-26 15:16:49 +0800

    initialize the method, shareTweet

--- lib/features/tweet/controller/tweet_controller.dart
+++ lib/features/tweet/controller/tweet_controller.dart
@@ -1,5 +1,14 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class TweetController extends StateNotifier<bool> {
   TweetController() : super(false);
+
+  void shareTweet({
+    required List<File> images,
+    required String text,
+    required BuildContext context,
+  }) {}
 }
