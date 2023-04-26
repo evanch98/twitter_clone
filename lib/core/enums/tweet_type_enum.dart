028fa43b19598ba309f8b9fa9b053ce3bb4c4882
Kyaw Thu <evanch98@gmail.com>
2023-04-26 16:05:05 +0800

    add ConvertTweet extension on String to convertthe String to TweetType Enum

--- lib/core/enums/tweet_type_enum.dart
+++ lib/core/enums/tweet_type_enum.dart
@@ -5,3 +5,17 @@ enum TweetType {
   final String type;
   const TweetType(this.type);
 }
+
+// convert the String to TweetType Enum
+extension ConvertTweet on String {
+  TweetType toEnum() {
+    switch(this) {
+      case 'text':
+        return TweetType.text;
+      case 'image':
+        return TweetType.image;
+      default:
+        return TweetType.text;
+    }
+  }
+}
