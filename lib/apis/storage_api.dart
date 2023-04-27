71be864b39cfd660b04aae00d6e0028b8f4808f5
Kyaw Thu <evanch98@gmail.com>
2023-04-27 16:09:11 +0800

    add StorageAPI and its method uploadImage

---
+++ lib/apis/storage_api.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:appwrite/appwrite.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/constants/constants.dart';
+import 'package:twitter_clone/core/core.dart';
+
+class StorageAPI {
+  final Storage _storage;
+
+  StorageAPI({required Storage storage}) : _storage = storage;
+
+  Future<List<String>> uploadImage(List<File> files) async {
+    List<String> imageLinks = [];
+    for (final file in files) {
+      final uploadedImage = await _storage.createFile(
+        bucketId: AppwriteConstants.imagesBucket,
+        fileId: ID.unique(),
+        file: InputFile.fromPath(path: file.path),
+      );
+      imageLinks.add(uploadedImage.$id);
+    }
+    return imageLinks;
+  }
+}
