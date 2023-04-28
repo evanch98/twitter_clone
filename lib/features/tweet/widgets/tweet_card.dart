1c516b0b294ac10092308fb566b39b0f6a4ad4d2
Kyaw Thu <evanch98@gmail.com>
2023-04-28 19:51:15 +0800

    add a required parameter tweet of type Tweet

--- lib/features/tweet/widgets/tweet_card.dart
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/models/models.dart';
 
 class TweetCard extends ConsumerWidget {
+  final Tweet tweet;
+
   const TweetCard({
     Key? key,
+    required this.tweet,
   }) : super(key: key);
 
   @override
