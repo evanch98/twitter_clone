b429c6476c9f5ca8ce0daabc5f2fd0637fb66392
Kyaw Thu <evanch98@gmail.com>
2023-04-28 23:01:02 +0800

    add three required parameters, pathName, text, and onTap

--- lib/features/tweet/widgets/tweet_icon_button.dart
+++ lib/features/tweet/widgets/tweet_icon_button.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
 class TweetIconButton extends StatelessWidget {
-  const TweetIconButton({Key? key}) : super(key: key);
+  final String pathName;
+  final String text;
+  final VoidCallback onTap;
+
+  const TweetIconButton({
+    Key? key,
+    required this.pathName,
+    required this.text,
+    required this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
-
