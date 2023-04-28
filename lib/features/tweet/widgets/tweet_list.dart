a9fe0eba013c9af9fef73d8fa6f3e11aedf1095a
Kyaw Thu <evanch98@gmail.com>
2023-04-29 00:52:34 +0800

    add the logic for the TweetList to update the tweets in realtime

--- lib/features/tweet/widgets/tweet_list.dart
+++ lib/features/tweet/widgets/tweet_list.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/constants/constants.dart';
 import 'package:twitter_clone/features/tweet/controller/tweet_controller.dart';
 import 'package:twitter_clone/features/tweet/widgets/tweet_card.dart';
+import 'package:twitter_clone/models/models.dart';
 
 class TweetList extends ConsumerWidget {
   const TweetList({
@@ -13,13 +15,39 @@ class TweetList extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return ref.watch(getTweetsProvider).when(
           data: (tweets) {
-            return ListView.builder(
-              itemCount: tweets.length,
-              itemBuilder: (BuildContext context, int index) {
-                final tweet = tweets[index];
-                return TweetCard(tweet: tweet);
-              },
-            );
+            return ref.watch(getLatestTweetProvider).when(
+                  data: (data) {
+                    if (data.events.contains(
+                      'databases.*.collections.${AppwriteConstants.tweetsCollection}.documents.*.create',
+                    )) {
+                      // to insert the new tweet at the top of the list
+                      tweets.insert(0, Tweet.fromMap(data.payload));
+                    }
+                    return ListView.builder(
+                      itemCount: tweets.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final tweet = tweets[index];
+                        return TweetCard(tweet: tweet);
+                      },
+                    );
+                  },
+                  error: (error, st) => ErrorText(
+                    error: error.toString(),
+                  ),
+                  loading: () {
+                    // the reason for this is due to the way appwrite works
+                    // until there is an update, it will keep loading
+                    // therefore, one of the way to solve this problem is to
+                    // return the same ListView.builder in the state of loading
+                    return ListView.builder(
+                      itemCount: tweets.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        final tweet = tweets[index];
+                        return TweetCard(tweet: tweet);
+                      },
+                    );
+                  },
+                );
           },
           error: (error, st) => ErrorText(
             error: error.toString(),
