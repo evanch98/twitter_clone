b12152e2d6c399d42556bbfbd78fc86b5cdade46
Kyaw Thu <evanch98@gmail.com>
2023-04-28 19:49:34 +0800

    add TweetCard (ConsumerWidget)

---
+++ lib/features/tweet/widgets/tweet_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class TweetCard extends ConsumerWidget {
+  const TweetCard({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
