9132eeb4f0568a153cfe3705639d407ff7d063ad
Kyaw Thu <evanch98@gmail.com>
2023-04-28 21:47:46 +0800

    change the height of the CarouselOptions

--- lib/features/tweet/widgets/carousel_image.dart
+++ lib/features/tweet/widgets/carousel_image.dart
@@ -39,7 +39,7 @@ class _CarouselImageState extends State<CarouselImage> {
                 },
               ).toList(),
               options: CarouselOptions(
-                  height: 400,
+                  height: 300,
                   viewportFraction: 1,
                   enableInfiniteScroll: false,
                   onPageChanged: (index, reason) {
