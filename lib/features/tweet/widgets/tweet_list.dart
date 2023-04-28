dc1e45c0c616a31b17c1b1b832223a8534630766
Kyaw Thu <evanch98@gmail.com>
2023-04-28 19:59:49 +0800

    instead of showing just a text, now TweetList will show TweetCard

--- lib/features/tweet/widgets/tweet_list.dart
+++ lib/features/tweet/widgets/tweet_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/common/common.dart';
 import 'package:twitter_clone/features/tweet/controller/tweet_controller.dart';
+import 'package:twitter_clone/features/tweet/widgets/tweet_card.dart';
 
 class TweetList extends ConsumerWidget {
   const TweetList({
@@ -16,7 +17,7 @@ class TweetList extends ConsumerWidget {
               itemCount: tweets.length,
               itemBuilder: (BuildContext context, int index) {
                 final tweet = tweets[index];
-                return Text(tweet.text);
+                return TweetCard(tweet: tweet);
               },
             );
           },
