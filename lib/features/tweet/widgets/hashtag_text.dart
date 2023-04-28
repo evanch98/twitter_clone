8a5687c52bca8584fbdaf608dd4e306ace22e88b
Kyaw Thu <evanch98@gmail.com>
2023-04-28 20:41:25 +0800

    add HashtagText to change the color of hashtags and links

---
+++ lib/features/tweet/widgets/hashtag_text.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:twitter_clone/theme/theme.dart';
+
+// To change the color of hashtags and links
+class HashtagText extends StatelessWidget {
+  final String text;
+
+  const HashtagText({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    List<TextSpan> textSpans = [];
+    text
+        .replaceAll("\n", " ")
+        .replaceAll("\t", " ")
+        .split(" ")
+        .forEach((element) {
+      if (element.startsWith("#")) {
+        textSpans.add(
+          TextSpan(
+            text: "$element ",
+            style: const TextStyle(
+              color: Pallete.blueColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      } else if (element.startsWith("www.") ||
+          element.startsWith("https://") ||
+          element.startsWith("http://")) {
+        textSpans.add(
+          TextSpan(
+            text: "$element ",
+            style: const TextStyle(
+              color: Pallete.blueColor,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      } else {
+        textSpans.add(
+          TextSpan(
+            text: "$element ",
+            style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        );
+      }
+    });
+    return RichText(
+      text: TextSpan(
+        children: textSpans,
+      ),
+    );
+  }
+}
