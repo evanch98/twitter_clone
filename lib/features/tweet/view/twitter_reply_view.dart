801a0d1db0355cfa0e57077a996fd8865ebba99b
Kyaw Thu <evanch98@gmail.com>
2023-05-01 22:49:38 +0800

    add an AppBar with a simple Text widget

--- lib/features/tweet/view/twitter_reply_view.dart
+++ lib/features/tweet/view/twitter_reply_view.dart
@@ -8,6 +8,10 @@ class ReplyTweetScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tweet'),
+      ),
+    );
   }
 }
