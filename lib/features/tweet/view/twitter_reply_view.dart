ff3f39b019d82b75c6960e02df534b227fa808ba
Kyaw Thu <evanch98@gmail.com>
2023-05-01 22:51:20 +0800

    add a route to the ReplyTweetScreen

--- lib/features/tweet/view/twitter_reply_view.dart
+++ lib/features/tweet/view/twitter_reply_view.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ReplyTweetScreen extends ConsumerWidget {
+  static route() => MaterialPageRoute(
+        builder: (context) => const ReplyTweetScreen(),
+      );
+
   const ReplyTweetScreen({
     Key? key,
   }) : super(key: key);
