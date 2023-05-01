b0f45c31e8029b5e915a928605de94e59d1bee88
Kyaw Thu <evanch98@gmail.com>
2023-05-01 22:52:41 +0800

    add a required parameter tweet of type Tweet

--- lib/features/tweet/view/twitter_reply_view.dart
+++ lib/features/tweet/view/twitter_reply_view.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/models/models.dart';
 
 class ReplyTweetScreen extends ConsumerWidget {
   static route() => MaterialPageRoute(
         builder: (context) => const ReplyTweetScreen(),
       );
 
+  final Tweet tweet;
+
   const ReplyTweetScreen({
     Key? key,
+    required this.tweet,
   }) : super(key: key);
 
   @override
