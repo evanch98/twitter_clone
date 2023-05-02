ad637e5479796e28c869af833cba14d65c99d4af
Kyaw Thu <evanch98@gmail.com>
2023-05-02 20:20:22 +0800

    add a ConsumerStatefulWidget ExploreView

---
+++ lib/features/explore/view/explore_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class ExploreView extends ConsumerStatefulWidget {
+  const ExploreView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  ConsumerState createState() => _ExploreViewState();
+}
+
+class _ExploreViewState extends ConsumerState<ExploreView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
