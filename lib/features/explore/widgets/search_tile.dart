ab4373f55997aefa6300932213528e9d51a6e15b
Kyaw Thu <evanch98@gmail.com>
2023-05-03 02:10:21 +0800

    add the subtitle of the ListTile

--- lib/features/explore/widgets/search_tile.dart
+++ lib/features/explore/widgets/search_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:twitter_clone/models/models.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class SearchTile extends StatelessWidget {
   final UserModel userModel;
@@ -22,6 +23,23 @@ class SearchTile extends StatelessWidget {
           fontSize: 18,
           fontWeight: FontWeight.w600,
         ),
+      ),
+      subtitle: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "@${userModel.name}",
+            style: const TextStyle(
+              fontSize: 16,
+            ),
+          ),
+          Text(
+            userModel.bio,
+            style: const TextStyle(
+              color: Pallete.whiteColor,
+            ),
+          )
+        ],
       ),
     );
   }
