d64351589191ca2ca44c3425b538e8fd6cced2f0
Kyaw Thu <evanch98@gmail.com>
2023-05-03 01:54:43 +0800

    add a new required parameter userModel of type UserModel

--- lib/features/explore/widgets/search_tile.dart
+++ lib/features/explore/widgets/search_tile.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:twitter_clone/models/models.dart';
 
 class SearchTile extends StatelessWidget {
-  const SearchTile({Key? key}) : super(key: key);
+  final UserModel userModel;
+
+  const SearchTile({
+    Key? key,
+    required this.userModel,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
