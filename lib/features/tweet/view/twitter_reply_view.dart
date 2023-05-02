14dd55d9c58d48741c1e01c6ba9d29e1f08aedd2
Kyaw Thu <evanch98@gmail.com>
2023-05-02 16:32:09 +0800

    making the UI of the reply section nicer

--- lib/features/tweet/view/twitter_reply_view.dart
+++ lib/features/tweet/view/twitter_reply_view.dart
@@ -106,15 +106,12 @@ class ReplyTweetScreen extends ConsumerWidget {
         padding: const EdgeInsets.all(8.0).copyWith(left: 15),
         child: Row(
           children: [
-            Container(
-              margin: const EdgeInsets.all(10),
-              child: currentUser == null ? const Loader() : CircleAvatar(
-                backgroundImage: NetworkImage(currentUser.profilePic),
-                radius: 25,
-              ),
+            currentUser == null ? const Loader() : CircleAvatar(
+              backgroundImage: NetworkImage(currentUser.profilePic),
+              radius: 25,
             ),
             const SizedBox(
-              width: 15,
+              width: 10,
             ),
             Expanded(
               child: TextField(
