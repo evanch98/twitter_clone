807289c98bb45f255d2e743f6f448f0be2adfdb8
Kyaw Thu <evanch98@gmail.com>
2023-05-03 01:41:34 +0800

    add a new method searchUser

--- lib/features/explore/controller/explore_controller.dart
+++ lib/features/explore/controller/explore_controller.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/apis/apis.dart';
+import 'package:twitter_clone/models/models.dart';
 
 class ExploreController extends StateNotifier<bool> {
   final UserAPI _userAPI;
@@ -8,4 +9,9 @@ class ExploreController extends StateNotifier<bool> {
     required UserAPI userAPI,
   })  : _userAPI = userAPI,
         super(false);
+
+  Future<List<UserModel>> searchUser(String name) async {
+    final users = await _userAPI.searchUserByName(name);
+    return users.map((e) => UserModel.fromMap(e.data)).toList();
+  }
 }
