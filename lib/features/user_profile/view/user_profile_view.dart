d6bbf0bbc02b055c6e5a16d5ad801564e4a480cc
Kyaw Thu <evanch98@gmail.com>
2023-05-03 16:14:08 +0800

    add a new required parameter userModel of type UserModel

--- lib/features/user_profile/view/user_profile_view.dart
+++ lib/features/user_profile/view/user_profile_view.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/models/models.dart';
 
 class UserProfileView extends ConsumerWidget {
+  final UserModel userModel;
   const UserProfileView({
     Key? key,
+    required this.userModel,
   }) : super(key: key);
 
   @override
