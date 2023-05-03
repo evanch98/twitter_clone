04cb075b60f1ae120acd92c3772f937137887a0b
Kyaw Thu <evanch98@gmail.com>
2023-05-03 19:20:15 +0800

    add a stateless widget FollowCount with two final fields count and text

---
+++ lib/features/user_profile/widget/follow_count.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class FollowCount extends StatelessWidget {
+  final String count;
+  final String text;
+
+  const FollowCount({
+    Key? key,
+    required this.count,
+    required this.text,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
