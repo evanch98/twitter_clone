ff16d2c48b14e9f03cae815e68ca7d64d2f1de29
Kyaw Thu <evanch98@gmail.com>
2023-05-03 16:12:13 +0800

    add a ConsumerWidget UserProfileView

---
+++ lib/features/user_profile/view/user_profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UserProfileView extends ConsumerWidget {
+  const UserProfileView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold();
+  }
+}
