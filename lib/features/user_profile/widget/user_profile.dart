d89fe790bab437a67a7c5a4d06dd1742feb7f742
Kyaw Thu <evanch98@gmail.com>
2023-05-03 16:25:33 +0800

    add the bannerPic on the UserProfileScreen

--- lib/features/user_profile/widget/user_profile.dart
+++ lib/features/user_profile/widget/user_profile.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/models/models.dart';
+import 'package:twitter_clone/theme/theme.dart';
 
 class UserProfile extends ConsumerWidget {
+  final UserModel userModel;
+
   const UserProfile({
     Key? key,
+    required this.userModel,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return NestedScrollView(
+      headerSliverBuilder: (context, innerBoxIsScrolled) {
+        return [
+          SliverAppBar(
+            expandedHeight: 150,
+            floating: true,
+            snap: true,
+            flexibleSpace: Stack(
+              children: [
+                Positioned.fill(
+                  child: userModel.bannerPic.isEmpty
+                      ? Container(
+                          color: Pallete.blueColor,
+                        )
+                      : Image.network(userModel.bannerPic),
+                ),
+              ],
+            ),
+          ),
+        ];
+      },
+      body: body,
+    );
   }
 }
