d696afe78fb2025648b8883bfaa0021f30ef6cb5
Kyaw Thu <evanch98@gmail.com>
2023-05-03 16:47:56 +0800

    reformat the code

--- lib/features/user_profile/widget/user_profile.dart
+++ lib/features/user_profile/widget/user_profile.dart
@@ -17,48 +17,50 @@ class UserProfile extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final currentUser = ref.watch(currentUserDetailsProvider).value;
 
-    return currentUser == null ? const Loader() : NestedScrollView(
-      headerSliverBuilder: (context, innerBoxIsScrolled) {
-        return [
-          SliverAppBar(
-            expandedHeight: 150,
-            floating: true,
-            snap: true,
-            flexibleSpace: Stack(
-              children: [
-                Positioned.fill(
-                  child: userModel.bannerPic.isEmpty
-                      ? Container(
-                          color: Pallete.blueColor,
-                        )
-                      : Image.network(userModel.bannerPic),
-                ),
-                Positioned(
-                  bottom: 0,
-                  child: CircleAvatar(
-                    backgroundImage: NetworkImage(userModel.profilePic),
-                    radius: 45,
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.bottomRight,
-                  margin: const EdgeInsets.all(20),
-                  child: OutlinedButton(
-                    onPressed: () {},
-                    child: Text(
-                      "Follow",
-                      style: const TextStyle(
-                        color: Pallete.whiteColor,
+    return currentUser == null
+        ? const Loader()
+        : NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [
+                SliverAppBar(
+                  expandedHeight: 150,
+                  floating: true,
+                  snap: true,
+                  flexibleSpace: Stack(
+                    children: [
+                      Positioned.fill(
+                        child: userModel.bannerPic.isEmpty
+                            ? Container(
+                                color: Pallete.blueColor,
+                              )
+                            : Image.network(userModel.bannerPic),
                       ),
-                    ),
+                      Positioned(
+                        bottom: 0,
+                        child: CircleAvatar(
+                          backgroundImage: NetworkImage(userModel.profilePic),
+                          radius: 45,
+                        ),
+                      ),
+                      Container(
+                        alignment: Alignment.bottomRight,
+                        margin: const EdgeInsets.all(20),
+                        child: OutlinedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Follow",
+                            style: const TextStyle(
+                              color: Pallete.whiteColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-              ],
-            ),
-          ),
-        ];
-      },
-      body: Container(),
-    );
+              ];
+            },
+            body: Container(),
+          );
   }
 }
