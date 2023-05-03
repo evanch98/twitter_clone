d405cde81f7e7c27dc7e438e3c31f9332f912b38
Kyaw Thu <evanch98@gmail.com>
2023-05-03 16:47:37 +0800

    add a logic to show the Loader() if the currentUser is null

--- lib/features/user_profile/widget/user_profile.dart
+++ lib/features/user_profile/widget/user_profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:twitter_clone/common/common.dart';
+import 'package:twitter_clone/features/auth/controller/auth_controller.dart';
 import 'package:twitter_clone/models/models.dart';
 import 'package:twitter_clone/theme/theme.dart';
 
@@ -13,7 +15,9 @@ class UserProfile extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return NestedScrollView(
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
+
+    return currentUser == null ? const Loader() : NestedScrollView(
       headerSliverBuilder: (context, innerBoxIsScrolled) {
         return [
           SliverAppBar(
