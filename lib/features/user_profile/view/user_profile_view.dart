d1476b41a6e45c53b2a9598f5ec795154a45ca93
Kyaw Thu <evanch98@gmail.com>
2023-05-03 16:16:31 +0800

    add a MaterialPageRoute to the UserProfileView

--- lib/features/user_profile/view/user_profile_view.dart
+++ lib/features/user_profile/view/user_profile_view.dart
@@ -1,9 +1,16 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/models/models.dart';
 
 class UserProfileView extends ConsumerWidget {
+  static route(UserModel userModel) => MaterialPageRoute(
+        builder: (context) => UserProfileView(userModel: userModel),
+      );
+
   final UserModel userModel;
+
   const UserProfileView({
     Key? key,
     required this.userModel,
@@ -12,7 +19,9 @@ class UserProfileView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: UserProfileView(userModel: userModel,),
+      body: UserProfileView(
+        userModel: userModel,
+      ),
     );
   }
 }
