e8969560402c1a1edbf5e3b05deeec39023d79cc
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:28:56 +0800

    convert to double quoted strings

--- lib/features/user_profile/view/edit_profile_view.dart
+++ lib/features/user_profile/view/edit_profile_view.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 
 class EditProfileView extends ConsumerStatefulWidget {
   const EditProfileView({
