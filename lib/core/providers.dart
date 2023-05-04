37d9a6224cb450a8052dd800cd4f800de26ab935
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:21:15 +0800

    convert to double quoted strings

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -1,6 +1,6 @@
-import 'package:appwrite/appwrite.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:twitter_clone/constants/constants.dart';
+import "package:appwrite/appwrite.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:twitter_clone/constants/constants.dart";
 
 final appwriteClientProvider = Provider.autoDispose((ref) {
   Client client = Client();
