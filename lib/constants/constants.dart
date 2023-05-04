794bebc00ba6d501dffdc680df3182a654c30d5d
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:19:12 +0800

    convert to double quoted strings

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,5 +1,5 @@
 // to avoid importing these files
 // we can just use import constants.dart
-export './appwrite_constants.dart';
-export './assets_constants.dart';
-export './ui_constants.dart';
+export "./appwrite_constants.dart";
+export "./assets_constants.dart";
+export "./ui_constants.dart";
