6b6f2548e9b9568fc3fae7665626f040615806e2
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:21:35 +0800

    convert to double quoted strings

--- lib/core/type_defs.dart
+++ lib/core/type_defs.dart
@@ -1,5 +1,5 @@
-import 'package:fpdart/fpdart.dart';
-import 'package:twitter_clone/core/failure.dart';
+import "package:fpdart/fpdart.dart";
+import "package:twitter_clone/core/failure.dart";
 
 // typedef simply allows us to create our own type definition
 // Either lets allows us to return two data types from a function (from the
