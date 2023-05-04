c7a761ae13661c28656346abd6fa4f8817d65f1f
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:10:59 +0800

    add a ConsumerStatefulWidget EditProfileView

---
+++ lib/features/user_profile/view/edit_profile_view.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class EditProfileView extends ConsumerStatefulWidget {
+  const EditProfileView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  ConsumerState createState() => _EditProfileViewState();
+}
+
+class _EditProfileViewState extends ConsumerState<EditProfileView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
