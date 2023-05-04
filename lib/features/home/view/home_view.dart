eaf4ec6330d1f14989fcbd7df8c98f65026bd258
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:25:22 +0800

    convert to double quoted strings

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_svg/svg.dart';
-import 'package:twitter_clone/constants/constants.dart';
-import 'package:twitter_clone/features/tweet/view/create_tweet_view.dart';
-import 'package:twitter_clone/theme/theme.dart';
+import "package:flutter/cupertino.dart";
+import "package:flutter/material.dart";
+import "package:flutter_svg/svg.dart";
+import "package:twitter_clone/constants/constants.dart";
+import "package:twitter_clone/features/tweet/view/create_tweet_view.dart";
+import "package:twitter_clone/theme/theme.dart";
 
 class HomeView extends StatefulWidget {
   static route() => MaterialPageRoute(
