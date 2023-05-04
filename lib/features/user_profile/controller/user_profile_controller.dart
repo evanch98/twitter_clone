cf0b9f2e0c84b26a3024bc77f22d5c9a3bfa365c
Kyaw Thu <evanch98@gmail.com>
2023-05-04 18:40:16 +0800

    add a provider for the UserProfileController

--- lib/features/user_profile/controller/user_profile_controller.dart
+++ lib/features/user_profile/controller/user_profile_controller.dart
@@ -2,6 +2,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:twitter_clone/apis/apis.dart';
 import 'package:twitter_clone/models/models.dart';
 
+final userProfileControllerProvider = StateNotifierProvider.autoDispose((ref) {
+  final tweetAPI = ref.watch(tweetAPIProvider);
+  return UserProfileController(tweetAPI: tweetAPI);
+});
+
 class UserProfileController extends StateNotifier<bool> {
   final TweetAPI _tweetAPI;
 
