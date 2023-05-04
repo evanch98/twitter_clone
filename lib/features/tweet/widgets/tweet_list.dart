49e6856921c3622987e9403cb40be10d70d56fb2
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:28:21 +0800

    convert to double quoted strings

--- lib/features/tweet/widgets/tweet_list.dart
+++ lib/features/tweet/widgets/tweet_list.dart
@@ -1,10 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:twitter_clone/common/common.dart';
-import 'package:twitter_clone/constants/constants.dart';
-import 'package:twitter_clone/features/tweet/controller/tweet_controller.dart';
-import 'package:twitter_clone/features/tweet/widgets/tweet_card.dart';
-import 'package:twitter_clone/models/models.dart';
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:twitter_clone/common/common.dart";
+import "package:twitter_clone/constants/constants.dart";
+import "package:twitter_clone/features/tweet/controller/tweet_controller.dart";
+import "package:twitter_clone/features/tweet/widgets/tweet_card.dart";
+import "package:twitter_clone/models/models.dart";
 
 class TweetList extends ConsumerWidget {
   const TweetList({
@@ -18,18 +18,18 @@ class TweetList extends ConsumerWidget {
             return ref.watch(getLatestTweetProvider).when(
                   data: (data) {
                     if (data.events.contains(
-                      'databases.*.collections.${AppwriteConstants.tweetsCollection}.documents.*.create',
+                      "databases.*.collections.${AppwriteConstants.tweetsCollection}.documents.*.create",
                     )) {
                       // to insert the new tweet at the top of the list
                       tweets.insert(0, Tweet.fromMap(data.payload));
                     } else if (data.events.contains(
-                      'databases.*.collections.${AppwriteConstants.tweetsCollection}.documents.*.update',
+                      "databases.*.collections.${AppwriteConstants.tweetsCollection}.documents.*.update",
                     )) {
                       // get id of the tweet
                       // to get the id of the old tweet, we need to find it in
                       // the date events[0] string
-                      final startingPoint = data.events[0].lastIndexOf('documents.');
-                      final endPoint = data.events[0].lastIndexOf('.update');
+                      final startingPoint = data.events[0].lastIndexOf("documents.");
+                      final endPoint = data.events[0].lastIndexOf(".update");
                       // startingPoint + 10 because we do not want to include
                       // 'documents.' and its length is 10
                       final tweetId = data.events[0].substring(startingPoint + 10, endPoint);
