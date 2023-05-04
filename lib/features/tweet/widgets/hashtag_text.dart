afab5ef285ec07b42ea24192ebccc82291148fe9
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:27:26 +0800

    convert to double quoted strings

--- lib/features/tweet/widgets/hashtag_text.dart
+++ lib/features/tweet/widgets/hashtag_text.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
-import 'package:twitter_clone/theme/theme.dart';
+import "package:flutter/material.dart";
+import "package:twitter_clone/theme/theme.dart";
 
 // To change the color of hashtags and links
 class HashtagText extends StatelessWidget {
