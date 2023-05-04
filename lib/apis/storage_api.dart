2d476f3ab38b799364d124e0ca886fadaceaff55
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:15:50 +0800

    convert to double quoted strings

--- lib/apis/storage_api.dart
+++ lib/apis/storage_api.dart
@@ -1,9 +1,9 @@
-import 'dart:io';
+import "dart:io";
 
-import 'package:appwrite/appwrite.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:twitter_clone/constants/constants.dart';
-import 'package:twitter_clone/core/core.dart';
+import "package:appwrite/appwrite.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:twitter_clone/constants/constants.dart";
+import "package:twitter_clone/core/core.dart";
 
 final storageAPIProvider = Provider.autoDispose((ref) {
   final storage = ref.watch(appwriteStorageProvider);
