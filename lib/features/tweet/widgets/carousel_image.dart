f55903969f9ac91fca1cfbe639c0fffa1423165b
Kyaw Thu <evanch98@gmail.com>
2023-05-04 20:27:03 +0800

    convert to double quoted strings

--- lib/features/tweet/widgets/carousel_image.dart
+++ lib/features/tweet/widgets/carousel_image.dart
@@ -1,5 +1,5 @@
-import 'package:carousel_slider/carousel_slider.dart';
-import 'package:flutter/material.dart';
+import "package:carousel_slider/carousel_slider.dart";
+import "package:flutter/material.dart";
 
 class CarouselImage extends StatefulWidget {
   final List<String> imageLinks;
