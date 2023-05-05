0cc0010ce623f53dfc5b5b9e09486cb7debf1e87
Kyaw Thu <evanch98@gmail.com>
2023-05-05 19:29:16 +0800

    add and initialize a new method updateUserProfile

--- lib/features/user_profile/controller/user_profile_controller.dart
+++ lib/features/user_profile/controller/user_profile_controller.dart
@@ -1,3 +1,6 @@
+import "dart:io";
+
+import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:twitter_clone/apis/apis.dart";
 import "package:twitter_clone/models/models.dart";
@@ -29,5 +32,26 @@ class UserProfileController extends StateNotifier<bool> {
   Future<List<Tweet>> getUserTweets(String uid) async {
     final tweets = await _tweetAPI.getUserTweets(uid);
     return tweets.map((e) => Tweet.fromMap(e.data)).toList();
+  }
+
+  void updateUserProfile({
+    required UserModel userModel,
+    required BuildContext context,
+    required File? bannerFile,
+    required File? profileFile,
+  }) async {
+    if (bannerFile != null) {
+      final bannerUrl = await _storageAPI.uploadImage([bannerFile]);
+      userModel = userModel.copyWith(
+        bannerPic: bannerUrl[0],
+      );
+    }
+
+    if (profileFile != null) {
+      final profileUrl = await _storageAPI.uploadImage([profileFile]);
+      userModel = userModel.copyWith(
+        profilePic: profileUrl[0],
+      );
+    }
   }
 }
