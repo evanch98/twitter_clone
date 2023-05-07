12e0199f6c863bf52d0ebd2871431bdf2bcbb6e7
Kyaw Thu <evanch98@gmail.com>
2023-05-07 23:23:20 +0800

    implement the abstract class INotificationAPI and its abstract method

--- lib/apis/notification_api.dart
+++ lib/apis/notification_api.dart
@@ -1,6 +1,45 @@
-// interface for the NotificationAPI
+import "package:appwrite/appwrite.dart";
+import "package:fpdart/fpdart.dart";
+import "package:twitter_clone/constants/constants.dart";
 import "package:twitter_clone/core/core.dart";
+import "package:twitter_clone/models/models.dart";
 
+// interface for the NotificationAPI
 abstract class INotificationAPI {
-  FutureEitherVoid createNotification();
+  FutureEitherVoid createNotification(Notification notification);
+}
+
+class NotificationAPI implements INotificationAPI {
+  final Databases _db;
+
+  NotificationAPI({
+    required Databases db,
+  }) : _db = db;
+
+  @override
+  FutureEitherVoid createNotification(Notification notification) async {
+    try {
+      await _db.createDocument(
+        databaseId: AppwriteConstants.databaseId,
+        collectionId: AppwriteConstants.notificationsCollection,
+        documentId: ID.unique(),
+        data: notification.toMap(),
+      );
+      return right(null);
+    } on AppwriteException catch (e, st) {
+      return left(
+        Failure(
+          e.message ?? "Some unexpected error occurred",
+          st,
+        ),
+      );
+    } catch (e, st) {
+      return left(
+        Failure(
+          e.toString(),
+          st,
+        ),
+      );
+    }
+  }
 }
