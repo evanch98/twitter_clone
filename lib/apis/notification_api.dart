19fb741b07e3a0942e48083b8b30fcaa4cc1fcd4
Kyaw Thu <evanch98@gmail.com>
2023-05-08 00:45:27 +0800

    add a new required parameter realtime of type Realtime

--- lib/apis/notification_api.dart
+++ lib/apis/notification_api.dart
@@ -8,7 +8,8 @@ import "package:twitter_clone/models/models.dart";
 
 final notificationAPIProvider = Provider.autoDispose((ref) {
   final db = ref.watch(appwriteDatabaseProvider);
-  return NotificationAPI(db: db);
+  final realtime = ref.watch(appwriteRealtimeProviderForNotification);
+  return NotificationAPI(db: db, realtime: realtime);
 });
 
 // interface for the NotificationAPI
@@ -20,10 +21,13 @@ abstract class INotificationAPI {
 
 class NotificationAPI implements INotificationAPI {
   final Databases _db;
+  final Realtime _realtime;
 
   NotificationAPI({
     required Databases db,
-  }) : _db = db;
+    required Realtime realtime,
+  })  : _db = db,
+        _realtime = realtime;
 
   @override
   FutureEitherVoid createNotification(Notification notification) async {
