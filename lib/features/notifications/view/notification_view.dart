6cf79de0a1b2707a0c1749dc727149015891e388
Kyaw Thu <evanch98@gmail.com>
2023-05-08 01:23:49 +0800

    add a logic to show the tweet in realtime: UI have to be updated later

--- lib/features/notifications/view/notification_view.dart
+++ lib/features/notifications/view/notification_view.dart
@@ -1,5 +1,10 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:twitter_clone/common/common.dart";
+import "package:twitter_clone/constants/constants.dart";
+import "package:twitter_clone/features/auth/controller/auth_controller.dart";
+import "package:twitter_clone/features/notifications/controller/notification_controller.dart";
+import "package:twitter_clone/models/models.dart" as model;
 
 class NotificationView extends ConsumerWidget {
   const NotificationView({
@@ -8,10 +13,55 @@ class NotificationView extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
     return Scaffold(
       appBar: AppBar(
         title: const Text("Notifications"),
       ),
+      body: currentUser == null
+          ? const Loader()
+          : ref.watch(getNotificationsProvider(currentUser.uid)).when(
+                data: (notifications) {
+                  return ref.watch(getLatestNotificationProvider).when(
+                        data: (data) {
+                          if (data.events.contains(
+                            "databases.*.collections.${AppwriteConstants.notificationsCollection}.documents.*.create",
+                          )) {
+                            // to insert the new tweet at the top of the list
+                            notifications.insert(
+                                0, model.Notification.fromMap(data.payload));
+                          }
+                          return ListView.builder(
+                            itemCount: notifications.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              final notification = notifications[index];
+                              return Text(notification.toString());
+                            },
+                          );
+                        },
+                        error: (error, st) => ErrorText(
+                          error: error.toString(),
+                        ),
+                        loading: () {
+                          // the reason for this is due to the way appwrite works
+                          // until there is an update, it will keep loading
+                          // therefore, one of the way to solve this problem is to
+                          // return the same ListView.builder in the state of loading
+                          return ListView.builder(
+                            itemCount: notifications.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              final notification = notifications[index];
+                              return Text(notification.toString());
+                            },
+                          );
+                        },
+                      );
+                },
+                error: (error, st) => ErrorText(
+                  error: error.toString(),
+                ),
+                loading: () => const Loader(),
+              ),
     );
   }
 }
