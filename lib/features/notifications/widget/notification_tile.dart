d97438a1a202ba75f89f51b84bc80b1ae3623cb1
Kyaw Thu <evanch98@gmail.com>
2023-05-08 01:38:44 +0800

    add a logic to show the appropriate icon for the ListTile's leading

--- lib/features/notifications/widget/notification_tile.dart
+++ lib/features/notifications/widget/notification_tile.dart
@@ -1,5 +1,9 @@
 import "package:flutter/material.dart";
+import "package:flutter_svg/flutter_svg.dart";
+import "package:twitter_clone/constants/assets_constants.dart";
+import "package:twitter_clone/core/core.dart";
 import "package:twitter_clone/models/models.dart" as model;
+import "package:twitter_clone/theme/theme.dart";
 
 class NotificationTile extends StatelessWidget {
   final model.Notification notification;
@@ -11,6 +15,40 @@ class NotificationTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ListTile(
+      leading: notification.notificationType == NotificationType.follow
+          ? const Icon(
+              Icons.person,
+              color: Pallete.blueColor,
+            )
+          : notification.notificationType == NotificationType.like
+              ? SvgPicture.asset(
+                  AssetsConstants.likeFilledIcon,
+                  colorFilter: const ColorFilter.mode(
+                    Pallete.redColor,
+                    BlendMode.srcIn,
+                  ),
+                  height: 20,
+                )
+              : notification.notificationType == NotificationType.retweet
+                  ? SvgPicture.asset(
+                      AssetsConstants.retweetIcon,
+                      colorFilter: const ColorFilter.mode(
+                        Pallete.whiteColor,
+                        BlendMode.srcIn,
+                      ),
+                      height: 20,
+                    )
+                  : notification.notificationType == NotificationType.reply
+                      ? SvgPicture.asset(
+                          AssetsConstants.commentIcon,
+                          colorFilter: const ColorFilter.mode(
+                            Pallete.whiteColor,
+                            BlendMode.srcIn,
+                          ),
+                          height: 20,
+                        )
+                      : null,
+    );
   }
 }
