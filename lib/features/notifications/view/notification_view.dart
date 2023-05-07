05c4b95bd90ab3c23ad7ee0acd36db9ab68e57f2
Kyaw Thu <evanch98@gmail.com>
2023-05-08 01:40:50 +0800

    update the UI of the notification

--- lib/features/notifications/view/notification_view.dart
+++ lib/features/notifications/view/notification_view.dart
@@ -4,6 +4,7 @@ import "package:twitter_clone/common/common.dart";
 import "package:twitter_clone/constants/constants.dart";
 import "package:twitter_clone/features/auth/controller/auth_controller.dart";
 import "package:twitter_clone/features/notifications/controller/notification_controller.dart";
+import "package:twitter_clone/features/notifications/widget/notification_tile.dart";
 import "package:twitter_clone/models/models.dart" as model;
 
 class NotificationView extends ConsumerWidget {
@@ -41,7 +42,9 @@ class NotificationView extends ConsumerWidget {
                             itemCount: notifications.length,
                             itemBuilder: (BuildContext context, int index) {
                               final notification = notifications[index];
-                              return Text(notification.toString());
+                              return NotificationTile(
+                                notification: notification,
+                              );
                             },
                           );
                         },
@@ -57,7 +60,9 @@ class NotificationView extends ConsumerWidget {
                             itemCount: notifications.length,
                             itemBuilder: (BuildContext context, int index) {
                               final notification = notifications[index];
-                              return Text(notification.toString());
+                              return NotificationTile(
+                                notification: notification,
+                              );
                             },
                           );
                         },
