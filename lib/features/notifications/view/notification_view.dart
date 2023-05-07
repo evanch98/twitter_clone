44a1e83ae5410d2538d846f6f8ab3b58c4b9756f
Kyaw Thu <evanch98@gmail.com>
2023-05-08 00:31:46 +0800

    add a ConsumerWidget, NotificationView

---
+++ lib/features/notifications/view/notification_view.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+class NotificationView extends ConsumerWidget {
+  const NotificationView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
