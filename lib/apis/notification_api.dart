d9edc3fcc9d7c410f6f457b95767cc49598e1bac
Kyaw Thu <evanch98@gmail.com>
2023-05-07 23:54:36 +0800

    add a provider for the NotificationAPI

--- lib/apis/notification_api.dart
+++ lib/apis/notification_api.dart
@@ -1,8 +1,14 @@
 import "package:appwrite/appwrite.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
 import "package:fpdart/fpdart.dart";
 import "package:twitter_clone/constants/constants.dart";
 import "package:twitter_clone/core/core.dart";
 import "package:twitter_clone/models/models.dart";
+
+final notificationAPIProvider = Provider.autoDispose((ref) {
+  final db = ref.watch(appwriteDatabaseProvider);
+  return NotificationAPI(db: db);
+});
 
 // interface for the NotificationAPI
 abstract class INotificationAPI {
