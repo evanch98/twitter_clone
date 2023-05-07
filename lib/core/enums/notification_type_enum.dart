3e4cb0f4f12c58e54bc689dfa02a497ce644f8bb
Kyaw Thu <evanch98@gmail.com>
2023-05-07 22:41:18 +0800

    add more NotificationTypes and extension ConvertNotification on String

--- lib/core/enums/notification_type_enum.dart
+++ lib/core/enums/notification_type_enum.dart
@@ -1,7 +1,25 @@
 enum NotificationType {
   like("like"),
-  reply("reply");
+  reply("reply"),
+  follow("follow"),
+  retweet("retweet");
+
 
   final String type;
   const NotificationType(this.type);
+}
+
+extension ConvertNotification on String {
+  NotificationType toNotificationTypeEnum() {
+    switch (this) {
+      case "retweet":
+        return NotificationType.retweet;
+      case "follow":
+        return NotificationType.follow;
+      case "reply":
+        return NotificationType.reply;
+      default:
+        return NotificationType.like;
+    }
+  }
 }
