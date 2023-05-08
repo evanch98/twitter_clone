1eaf2d182794bc0dccf48b3ebb351c3d67d91eb1
Kyaw Thu <evanch98@gmail.com>
2023-05-09 00:54:46 +0800

    get the currenUser model from the provider

--- lib/features/home/widget/side_drawer.dart
+++ lib/features/home/widget/side_drawer.dart
@@ -1,5 +1,7 @@
 import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
+import "package:twitter_clone/common/common.dart";
+import "package:twitter_clone/features/auth/controller/auth_controller.dart";
 import "package:twitter_clone/theme/theme.dart";
 
 class SideDrawer extends ConsumerWidget {
@@ -9,6 +11,10 @@ class SideDrawer extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final currentUser = ref.watch(currentUserDetailsProvider).value;
+    if (currentUser == null) {
+      return const Loader();
+    }
     return SafeArea(
       child: Drawer(
         backgroundColor: Pallete.backgroundColor,
