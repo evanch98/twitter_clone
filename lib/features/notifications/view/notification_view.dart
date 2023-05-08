4a943dacca0faa4df7cd935d3e85651510f0b85d
Kyaw Thu <evanch98@gmail.com>
2023-05-09 00:38:40 +0800

    rename the folder name from widget to widgets to be consistent with others

--- lib/features/notifications/view/notification_view.dart
+++ lib/features/notifications/view/notification_view.dart
@@ -4,7 +4,7 @@ import "package:twitter_clone/common/common.dart";
 import "package:twitter_clone/constants/constants.dart";
 import "package:twitter_clone/features/auth/controller/auth_controller.dart";
 import "package:twitter_clone/features/notifications/controller/notification_controller.dart";
-import "package:twitter_clone/features/notifications/widget/notification_tile.dart";
+import "package:twitter_clone/features/notifications/widgets/notification_tile.dart";
 import "package:twitter_clone/models/models.dart" as model;
 
 class NotificationView extends ConsumerWidget {
