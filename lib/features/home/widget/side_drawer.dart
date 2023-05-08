8470cff13504e8531d556f213bdbe9c13d34b0be
Kyaw Thu <evanch98@gmail.com>
2023-05-09 00:40:42 +0800

    add a ConsumerWidget SideDrawer

---
+++ lib/features/home/widget/side_drawer.dart
@@ -0,0 +1,13 @@
+import "package:flutter/material.dart";
+import "package:flutter_riverpod/flutter_riverpod.dart";
+
+class SideDrawer extends ConsumerWidget {
+  const SideDrawer({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
