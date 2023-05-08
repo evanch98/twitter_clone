2333b5b68560b338e76278b2466b8f85e5a956a9
Kyaw Thu <evanch98@gmail.com>
2023-05-08 23:59:54 +0800

    convert to the double quoted strings

--- test/utils_test.dart
+++ test/utils_test.dart
@@ -1,9 +1,9 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:twitter_clone/core/core.dart';
+import "package:flutter_test/flutter_test.dart";
+import "package:twitter_clone/core/core.dart";
 
 void main() {
-  test('The name equals to the email before the @ sign', () {
-    final String name = getNameFromEmail('joebloggs@email.com');
-    expect('joebloggs', name);
+  test("The name equals to the email before the @ sign", () {
+    final String name = getNameFromEmail("joebloggs@email.com");
+    expect("joebloggs", name);
   });
 }
