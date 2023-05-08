3aa665e35bf6a4e3758b2ae2758bcdc399182a10
Kyaw Thu <evanch98@gmail.com>
2023-05-09 00:17:30 +0800

    add an appBar and the title of the appBar

--- lib/features/tweet/view/hashtag_view.dart
+++ lib/features/tweet/view/hashtag_view.dart
@@ -2,12 +2,19 @@ import "package:flutter/material.dart";
 import "package:flutter_riverpod/flutter_riverpod.dart";
 
 class HashtagView extends ConsumerWidget {
+  final String hashtag;
+
   const HashtagView({
     Key? key,
+    required this.hashtag,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(hashtag),
+      ),
+    );
   }
 }
